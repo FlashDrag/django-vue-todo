d59d36c2dcb6f365d2b8a301ebf3b401bb07268d
flashdrag <flashdrag@gmail.com>
2023-12-29 12:27:12 +0000

    Define CORS_ALLOWED_ORIGINS dynamically

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,6 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    allowedHosts: [
-      "ec2-18-201-100-234.eu-west-1.compute.amazonaws.com",
-      "localhost",
-    ],
-  },
 });
+
